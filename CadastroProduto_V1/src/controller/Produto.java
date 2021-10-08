5322cd0941c6ab7cbb3e60c052a7859f9ff1ffaf
Gustavo-Ae <gustavoaedias05@hotmail.com>
2021-10-08 08:47:55 -0300

    Criando a classe do Produto

---
+++ CadastroProduto_V1/src/controller/Produto.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+
+public class Produto{
+    private String nome;
+    private int quantidade;
+    private double valor;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+}
